a8ed20b16401174da194b208a7d488e5a6528084
Scott Ivey <scott.ivey@gmail.com>
2017-12-11 01:35:29 -0500

    yolo

--- scripts/braid-git-fuckery.zsh
+++ scripts/braid-git-fuckery.zsh
@@ -3,24 +3,38 @@
 set -uo pipefail
 setopt err_return
 
-#
 . ${0:A:h}/_common.zsh
+
+
+
+cat - <<-'EOD'
+master/braid//home/scott/ncode/learnvimscriptthehardway-wrapper/external/sjl/bookmarkdown
+EOD
+
+# no version tags upstream
+UPSTREAM_BRANCH='master'
+
 () {
     local ext_d=${LVS_THWW__EXT_D}
     local gh_user='sjl'
     local user_url="https://github.com/${gh_user}"
     typeset -a repo_names ;
-    repo_names=(
-        'bookmarkdown'
-        'learnvimscriptthehardway'
-    )
+    repo_names=( 'bookmarkdown' 'learnvimscriptthehardway' )
     local curr="" upstream="" ours="" ;
+    typeset -a braid_call ;
     for curr in ${repo_names[@]}; do
         upstream="${user_url}/${curr}.git"
         ours=${ext_d}/${gh_user}/${curr}
-        mkdir -p ${ours:A:h} ;
-        # no version tags upstream
-        braid add "$upstream" "$ours" --branch=master
+        braid_call=(
+            'braid' 'add'
+            "$upstream"
+            "$ours"
+            "--branch=$UPSTREAM_BRANCH"
+        )
+        # mkdir -p ${ours:A:h} ;
+        lvthw::info "braid call is: " ;
+        ds-argshow-bin ${braid_call[@]} ;
+        # braid add "$upstream" "$ours" --branch=$UPSTREAM_BRANCH
     done
 } $@ ;
 
